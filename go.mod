9402a0776d682f80b489152906d199f8df571658
Kevin Klopfenstein <kevin.klopfenstein@bedag.ch>
2022-12-21 19:47:00 +0100

    adding go script

---
+++ go.mod
@@ -0,0 +1,55 @@
+module github.com/buttahtoast/ejsonmerger
+
+go 1.19
+
+require (
+	github.com/Shopify/ejson v1.3.3
+	github.com/spf13/viper v1.14.0
+	sigs.k8s.io/kustomize/api v0.12.1
+)
+
+require (
+	bazil.org/fuse v0.0.0-20221210232012-5a1c75a4f691 // indirect
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/blang/vfs v1.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dustin/gojson v0.0.0-20160307161227-2e71ec9dd5ad // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-errors/errors v1.0.1 // indirect
+	github.com/go-git/go-git v4.7.0+incompatible // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.5 // indirect
+	github.com/go-openapi/swag v0.19.14 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/gnostic v0.5.7-v3refs // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/otiai10/copy v1.9.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/afero v1.9.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/src-d/gcfg v1.4.0 // indirect
+	github.com/subosito/gotenv v1.4.1 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	golang.org/x/net v0.0.0-20221014081412-f15817d10f9b // indirect
+	golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/src-d/go-billy.v4 v4.3.2 // indirect
+	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20220401212409-b28bf2818661 // indirect
+	sigs.k8s.io/kustomize/kyaml v0.13.9 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/Shopify/ejson"
+	"github.com/spf13/viper"
+	"gopkg.in/src-d/go-billy.v4"
+	"gopkg.in/src-d/go-billy.v4/memfs"
+	"sigs.k8s.io/kustomize/api/konfig"
+	"sigs.k8s.io/kustomize/api/types"
+)
+
+// config
+type config struct {
+	TmpDir                string   `mapstructure:"TMP_DIR"`
+	RootDirectory         string   `mapstructure:"ROOT_DIRECTORY"`
+	ExtraDirectories      []string `mapstructure:"EXTRA_DIRECTORIES"`
+	EjsonFileRegex        string   `mapstructure:"EJSON_FILE_REGEX"`
+	EjsonSecret           string   `mapstructure:"EJSON_SECRET"`
+	EjsonInline           string   `mapstructure:"EJSON_INLINE_KEYS"`
+	VarFileRegex          string   `mapstructure:"VAR_FILE_REGEX"`
+	KustomizeBuildOptions string   `mapstructure:"KUSTOMIZE_BUILD_OPTIONS"`
+}
+
+var c config
+var paths []string
+var keys []string
+var tmp string
+var vfs billy.Filesystem
+
+func main() {
+	configure()
+	// merge extra_directories and root directories
+	paths = append(c.ExtraDirectories, c.RootDirectory)
+
+	// read kustomization resources and add path
+	kz, err := readKustomize()
+	if err != nil {
+		log.Println(err)
+		os.Exit(2)
+	}
+	paths = append(paths, kz.Resources...)
+
+	// TODO: Secret get from kubernetes secrets
+
+	// decrypt ejosn files
+	keys = append(keys, c.EjsonSecret)
+	err = filepath.Walk(c.RootDirectory, decryptEjsonFiles)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(3)
+	}
+
+	file, _ := vfs.OpenFile("bla/bla/blu/blu.ejson", os.O_RDONLY, os.ModePerm)
+	filebyte, _ := io.ReadAll(file)
+	fmt.Println(string(filebyte))
+	//fmt.Println(c.ExtraDirectories[0])
+
+}
+
+func decryptEjsonFiles(path string, info os.FileInfo, err error) error {
+	if filepath.Ext(path) == c.EjsonFileRegex {
+		// found file decrypt it to tmp dir
+		decrypt, _ := ejson.DecryptFile(path, "", keys[0])
+		file, err := vfs.Create(path)
+		if err != nil {
+			return err
+		}
+		decryptReader := bytes.NewReader(decrypt)
+		_, err = io.Copy(file, decryptReader)
+		if err != nil {
+			return err
+		}
+		return err
+	}
+
+	return err
+}
+
+func configure() {
+	v := viper.New()
+	// configure defaults
+	v.SetDefault("TMP_DIR", "/tmp")
+	v.SetDefault("KUSTOMIZE_BUILD_OPTIONS", "")
+	v.SetDefault("ROOT_DIRECTORY", ".")
+	v.SetDefault("EXTRA_DIRECTORIES", os.Getenv("ARGOCD_ENV_EXTRA_DIRECTORIES"))
+	v.SetDefault("EJSON_FILE_REGEX", ".ejson")
+	v.SetDefault("EJSON_SECRET", os.Getenv("ARGOCD_ENV_SECRET"))
+	v.SetDefault("EJSON_INLINE_KEYS", "")
+	v.SetDefault("VAR_FILE_REGEX", "*.vars")
+	v.AutomaticEnv()
+
+	err := v.Unmarshal(&c)
+	if err != nil {
+		fmt.Println(err)
+	}
+	v.Unmarshal(&c)
+	// create a memory filesystem
+	vfs = memfs.New()
+	filepath.Walk(c.RootDirectory, func(path string, info fs.FileInfo, err error) error {
+		if info.IsDir() {
+			return err
+		}
+		src, _ := os.OpenFile(path, os.O_RDONLY, os.ModePerm)
+		dst, err := vfs.Create(path)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		io.Copy(dst, src)
+		return err
+	})
+	//fmt.Println(vfs.Root())
+	// tmp, err = os.MkdirTemp("", "ejsonmerger")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)
+	// }
+	// cp.Copy(c.RootDirectory, tmp)
+	//defer os.Remove(tmp.Name())
+}
+
+func readKustomize() (types.Kustomization, error) {
+	kz := types.Kustomization{}
+	for _, kfilename := range konfig.RecognizedKustomizationFileNames() {
+		if _, err := os.Stat(kfilename); err == nil {
+			kzBytes, err := os.ReadFile(kfilename)
+			if err != nil {
+				return kz, err
+			}
+			err = kz.Unmarshal(kzBytes)
+
+			return kz, err
+		}
+	}
+	return kz, fmt.Errorf("no kustomization file found")
+}
